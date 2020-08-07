049c473ec1526c3e0b5181785e924c0857424cb3
Cleyson Lima <cleysonph@gmail.com>
2020-08-07 17:45:05 -0300

    Register the student model in the admin

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from school.models import Student
+
+
+@admin.register(Student)
+class StudentModelAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "rg", "cpf", "birth_date"]
+    list_display_link = ["id", "nome"]
+    search_fields = ["nome"]
+    list_per_page = 20

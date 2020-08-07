8fd72d2c4d790cdd1c3885cf63939da73ef601ca
Cleyson Lima <cleysonph@gmail.com>
2020-08-07 17:56:45 -0300

    Create the course serializer

--- school/serializer.py
+++ school/serializer.py
@@ -7,3 +7,9 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = "__all__"

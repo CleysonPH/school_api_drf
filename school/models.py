393149a0170f4f5259541bde9f684344e8aa0765
Cleyson Lima <cleysonph@gmail.com>
2020-08-07 14:33:52 -0300

    Create the student model

--- school/models.py
+++ school/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField("Nome", max_length=50)
+    rg = models.CharField("RG", max_length=9)
+    cpf = models.CharField("CPF", max_length=11)
+    birth_date = models.DateField(
+        "Data de Nascimento", auto_now=False, auto_now_add=False
+    )
+
+    class Meta:
+        verbose_name = "Aluno"
+        verbose_name_plural = "Alunos"
+
+    def __str__(self):
+        return self.name

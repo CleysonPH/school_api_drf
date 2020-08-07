520d26092571a28d34ad399a507abab0e1ca2f53
Cleyson Lima <cleysonph@gmail.com>
2020-08-07 17:49:47 -0300

    Register the course model in the admin

--- school/admin.py
+++ school/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 
-from school.models import Student
+from school.models import Student, Course
 
 
 @admin.register(Student)
 class StudentModelAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "rg", "cpf", "birth_date"]
-    list_display_link = ["id", "nome"]
-    search_fields = ["nome"]
+    list_display_links = ["id", "name"]
+    search_fields = ["name"]
     list_per_page = 20
+
+
+@admin.register(Course)
+class CourseModelAdmin(admin.ModelAdmin):
+    list_display = ["id", "code", "description"]
+    list_display_links = ["id", "code"]
+    search_fields = ["code"]
+

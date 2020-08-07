1640b5f772d227c58264ae3f4bf7def11a268bc4
Cleyson Lima <cleysonph@gmail.com>
2020-08-07 18:10:05 -0300

    Fix missing import

--- school/views.py
+++ school/views.py
@@ -1,18 +1,18 @@
 from rest_framework import viewsets
 
-from school.models import Student
-from school.serializer import StudentSerializer
+from school.models import Student, Course
+from school.serializer import StudentSerializer, CourseSerializer
 
 
 class StudentViewSet(viewsets.ModelViewSet):
-    """"Show all students in the database"""
+    """Show all students in the database"""
 
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
-    """"Show all courses in the database"""
+    """Show all courses in the database"""
 
     queryset = Course.objects.all()
     serializer_class = CourseSerializer

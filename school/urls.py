b57e7e955986614ed0c5a3a4ea4a20c361fabd2c
Cleyson Lima <cleysonph@gmail.com>
2020-08-07 19:15:10 -0300

    Create the urls for the registration viewset using router

--- school/urls.py
+++ school/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from school.views import StudentViewSet, CourseViewSet
+from school.views import StudentViewSet, CourseViewSet, RegistrationViewSet
 
 
 router = routers.DefaultRouter()
 router.register("students", StudentViewSet, basename="student")
 router.register("courses", CourseViewSet, basename="course")
+router.register("registrations", RegistrationViewSet, basename="registration")
 
 
 app_name = "school-api"

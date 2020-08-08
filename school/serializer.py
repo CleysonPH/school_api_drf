97067d8e6753638222dc67fd031134473dd92238
Cleyson Lima <cleysonph@gmail.com>
2020-08-07 22:20:42 -0300

    Create a serializer to show all registrations for a specific student

--- school/serializer.py
+++ school/serializer.py
@@ -19,3 +19,15 @@ class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registration
         fields = "__all__"
+
+
+class StudentRegistrationsSerializer(serializers.ModelSerializer):
+    course = serializers.ReadOnlyField(source="course.description")
+    period = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Registration
+        fields = ["course", "period"]
+
+    def get_period(self, obj):
+        return obj.get_period_display()
